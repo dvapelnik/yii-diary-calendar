c0098d88dc06a425be0254c2d2dc9d8c8e77f03e
Dmitry Vapelnik <dvapelnik@gmail.com>
2013-09-26 17:52:35 +0300

    All methods was realised

--- helpers/Date.php
+++ helpers/Date.php
@@ -10,53 +10,157 @@ class Date extends CComponent
     public $year;
     public $month;
     public $day;
+    public $dow;
+    public $dowVerboseShort;
+    public $dowVerboseLong;
 
     public function __construct($year, $month, $day = 1)
     {
-        $this->year = $year;
-        $this->month = $month;
-        $this->day = $day;
+//        $this->_formatter = new CDateFormatter(Yii::app()->language);
+
+        $this->year = (string)$year;
+        $this->month = (string)$month;
+        $this->day = (string)$day;
+        $this->dow = $this->getDayOfWeekNumber();
+        $this->dowVerboseShort = $this->getDeyOfWeekVerbose(false);
+        $this->dowVerboseLong = $this->getDeyOfWeekVerbose();
+
+        if(!$this->isDayInMonth())
+        {
+            throw new CException('Day is not in month');
+        }
+    }
+
+    public static function GetFromUNIX($timestamp)
+    {
+        $year = date('Y', $timestamp);
+        $month = date('n', $timestamp);
+        $day = date('d', $timestamp);
+
+        return new Date($year, $month, $day);
+    }
+
+    public function __toString()
+    {
+        return sprintf("%s-%s-%s", $this->year, $this->month, $this->day);
     }
 
     //region Prev-Next
     public function getPrevDay()
     {
+        return $this->subDays(1);
     }
 
     public function getNextDay()
     {
+        return $this->addDays(1);
     }
 
     public function getPrevMonth()
     {
+        return $this->subMonth(1);
     }
 
     public function getNextMonth()
     {
+        return $this->addMonth(1);
     }
 
     public function getPrevYear()
     {
+        return $this->subYear(1);
     }
 
     public function getNextYear()
     {
+        return $this->addYear(1);
+    }
+
+    //endregion
+
+    //region Add-Sub
+    public function addDays($days)
+    {
+        $timestamp = strtotime((string)$this);
+        $newTimestamp = $timestamp + $days * 24 * 60 * 60;
+
+        return self::GetFromUNIX($newTimestamp);
+    }
+
+    public function subDays($days)
+    {
+        $timestamp = strtotime((string)$this);
+        $newTimestamp = $timestamp - $days * 24 * 60 * 60;
+
+        return self::GetFromUNIX($newTimestamp);
+    }
+
+    public function addMonth($month)
+    {
+        $newDate = $this;
+        for($i = 1; $i <= $month; $i++)
+        {
+            $newDate = $newDate->addDays($newDate->getCountOfDaysInCurrentMonth());
+        }
+
+        return $newDate;
+    }
+
+    public function subMonth($month)
+    {
+        $newData = $this;
+        for($i = $month; $i >= 1; $i--)
+        {
+            $newData = $newData->subDays($newData->day + 1);
+            $newData = $newData->subDays($newData->getCountOfDaysInCurrentMonth() - 1 - $this->day);
+        }
+
+        return $newData;
+    }
+
+    public function addYear($year)
+    {
+        $newDate = $this;
+        for($i = 1; $i <= 12 * $year; $i++)
+        {
+            $newDate = $newDate->addMonth(1);
+        }
+
+        return $newDate;
+    }
+
+    public function subYear($year)
+    {
+        $newDate = $this;
+        for($i = 12 * $year; $i >= 1; $i--)
+        {
+            $newDate = $newDate->subMonth(1);
+        }
+
+        return $newDate;
     }
 
     //endregion
 
     public function generateDaysInCurrentMonth()
     {
+        $daysInMonth = array();
+        for($i = 1; $i <= $this->getCountOfDaysInCurrentMonth(); $i++)
+        {
+            $daysInMonth[] = new Date($this->year, $this->month, $i);
+        }
+
+        return $daysInMonth;
     }
 
     public function getDayOfWeekNumber()
     {
-        return jddayofweek($this->toJDCalendar(), 0);
+        return Yii::app()->dateFormatter->format('e', strtotime((string)$this));
     }
 
-    public function getDeyOfWeekVerbose($isShort = false)
+    public function getDeyOfWeekVerbose($isLong = true)
     {
-        return jddayofweek($this->toJDCalendar(), $isShort ? 2 : 1);
+        return Yii::app()->dateFormatter->format($isLong ? 'EEEE' : 'EEE', strtotime((string)$this));
     }
 
     protected function toJDCalendar()
@@ -101,4 +205,10 @@ class Date extends CComponent
     {
         return $this->month == 12 && $this->day == 31;
     }
+
+    protected function isDayInMonth()
+    {
+        return $this->day <= $this->getCountOfDaysInCurrentMonth();
+    }
+
 }
