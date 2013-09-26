e16187bf5995f748da1e2be33f34a5dde322b1a9
Dmitry Vapelnik <dvapelnik@gmail.com>
2013-09-26 03:33:33 +0300

    Some methods was realised

--- helpers/Date.php
+++ helpers/Date.php
@@ -51,29 +51,54 @@ class Date extends CComponent
 
     public function getDayOfWeekNumber()
     {
+        return jddayofweek($this->toJDCalendar(), 0);
     }
 
-    public function getDeyOfWeekVerbose()
+    public function getDeyOfWeekVerbose($isShort = false)
     {
+        return jddayofweek($this->toJDCalendar(), $isShort ? 2 : 1);
+    }
+
+    protected function toJDCalendar()
+    {
+        return gregoriantojd($this->month, $this->day, $this->year);
     }
 
     protected function getCountOfDaysInCurrentMonth()
     {
+        return cal_days_in_month(CAL_GREGORIAN, $this->month, $this->year);
     }
 
     protected function getNumOfDayInYear()
     {
+        $numDayInYear = 0;
+        for($month = 1; $month < $this->month; $month++)
+        {
+            $numDayInYear += cal_days_in_month(CAL_GREGORIAN, $month, $this->year);
+        }
+        $numDayInYear += $this->day;
+
+        return $numDayInYear;
     }
 
     protected function getCountOfDaysInYear()
     {
+        $countDaysInYear = 0;
+        for($month = 1; $month <= 12; $month++)
+        {
+            $countDaysInYear += cal_days_in_month(CAL_GREGORIAN, $month, $this->year);
+        }
+
+        return $countDaysInYear;
     }
 
     protected function isLastDayInMonth()
     {
+        return $this->day == $this->getCountOfDaysInCurrentMonth();
     }
 
     protected function isLastDayInYear()
     {
+        return $this->month == 12 && $this->day == 31;
     }
 }
