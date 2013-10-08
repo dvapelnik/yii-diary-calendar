d074d7b1689a502439e07d1bab7af63563662aa8
Dmitry Vapelnik <dvapelnik@gmail.com>
2013-10-09 01:05:35 +0300

    1. Event remove

--- views/default/edit.php
+++ views/default/edit.php
@@ -5,7 +5,22 @@
  * @var $form CActiveForm
  * @var $header string
  */
-
+Yii::app()->getClientScript()->registerCss(
+    'button-class',
+    ".button {
+    font: bold 11px Arial;
+    text-decoration: none;
+    background-color: #EEEEEE;
+    color: #333333;
+    padding: 2px 6px 2px 6px;
+    border-top: 1px solid #CCCCCC;
+    border-right: 1px solid #333333;
+    border-bottom: 1px solid #333333;
+    border-left: 1px solid #CCCCCC;
+    margin: 2px;
+   }",
+    'all'
+);
 $formId = 'cal-form';
 ?>
 
@@ -30,12 +45,25 @@ $formId = 'cal-form';
     <div style="text-align: right">
         <?php
         echo $form->hiddenField($calendarModel, 'timestamp');
+        echo CHtml::link(
+            'Remove',
+            Yii::app()->createUrl(
+                '/calendar/default/remove',
+                array(
+                    'id' => Yii::app()->request->getParam('id')
+                )
+            ),
+            array(
+                'style' => 'margin-right: 4px;'
+            )
+        );
         echo CHtml::submitButton(
             'Save',
             array(
                 'style' => 'text-align: right;',
             )
         );
+
         ?>
     </div>
     <?php $this->endWidget(); ?>
