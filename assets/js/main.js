00e4b67296e04bf14939b81d4958cfccc7d29b3e
Dmitry Vapelnik <dvapelnik@gmail.com>
2013-10-09 12:17:58 +0300

    1. assets/js/main.js set day-container height as max height on page ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,16 @@ $('body').on('click', 'a[href=#]', function (event)
 {
     return false;
 });
+
+$(document).on('ready', function (event)
+{
+    var maxHeight = 0;
+    $('.day-container').each(function (index, element)
+    {
+        var currentHeight = $(this).height();
+        if (maxHeight < currentHeight)
+        {
+            maxHeight = currentHeight;
+        }
+    }).height(maxHeight);
+});
